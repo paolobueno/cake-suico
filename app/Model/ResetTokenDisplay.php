f02c4e8eaa62bfe2de4f1e85d3b6e3bb4826d69a
Paolo Bueno <paolohaji@gmail.com>
2012-08-20 20:39:07 -0300

    comentários no modelo Display

--- app/Model/ResetTokenDisplay.php
+++ app/Model/ResetTokenDisplay.php
@@ -5,7 +5,12 @@
 */
 class ResetTokenDisplay extends AppModel
 {
+  // expomos a variável a seguir para indicar que nosso
+  // Model não utiliza uma tabela no banco de dados
   public $useTable = false;
+
+  // este modelo tem somente uma simples validação do e-mail
+  // para o formulário em app/View/ResetToken/index.ctp
   public $validate = array(
       'email' => array(
         'rule' => 'email',
